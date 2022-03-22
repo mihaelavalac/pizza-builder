c83c341519bc6ffce56aa6e16f4a2f31804b7313
Mihaela Valac <mvalac@student.cscc.edu>
2022-03-22 16:45:29 -0400

    Added reply to comments functionality.

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,16 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {addComment, removeComment} = require('../../controllers/comment-controller');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply,
+} = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
-router
-.route('/:pizzaId')
+router.route("/:pizzaId")
 .post(addComment);
 
-
 // /api/comments/<pizzaId>/<commentId>
-router
-.route('/:pizzaId/:commentId')
+router.route("/:pizzaId/:commentId")
+.put(addReply)
 .delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId")
+.delete(removeReply);
 
 module.exports = router;
