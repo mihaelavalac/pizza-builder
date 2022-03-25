d1e0976e0cf0a78c282fb53ffff57d067b143dee
Mihaela Valac <mvalac@student.cscc.edu>
2022-03-24 23:29:52 -0400

    test

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 //This file package up all the models.
-const Pizza = require('./Pizza');
-const Comment = require('./Comment');
+const Pizza = require('./Pizza.js');
+const Comment = require('./Comment.js');
 
 module.exports = { Pizza, Comment };
